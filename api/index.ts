fa34a0dc84d3d2708455798c528505b496598b5f
Pendaftaran <ahmadrifqi942987@gmail.com>
2025-05-28 11:22:50 +0700

    wrangler confit-gitnore

---
+++ api/index.ts
@@ -0,0 +1,18 @@
+interface Env {
+  ASSETS: Fetcher;
+  DB: D1Database;
+}
+
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/api/events")) {
+      
+    }
+      let { results } = await env.DB.prepare("SELECT * FROM events").all();
+      return Response.json(results);    
+
+    return env.ASSETS.fetch(request);   
+  },
+} satisfies ExportedHandler<Env>;   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+
+interface Event {
+  id: number
+  name: string
+  place: string
+  time: string
+}
+
+const events = ref<Event[]>([])
+
+const fetchEvents = async () => {
+  const response = await fetch('/api/events')
+  const data = await response.json()
+  events.value = data
+}
+
+onMounted(() => {
+  fetchEvents()
+})
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    Home
+    <div>Daftar Event</div>
+
+    <div>
+      <ul>
+        <li v-for="event in events" :key="event.id">
+          <div>{{ event.name }}</div>
+          <div>{{ event.place }}</div>
+          <div>{{ event.time }}</div>
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
